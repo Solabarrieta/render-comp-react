24f268bd4674aef6570b6c0b7f88aa936b578bfa
oier <oieruni22@gmail.com>
2024-03-15 12:41:40 +0100

    [ADD] Playing with HOC (High Order Components)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// function App(props) {
+//   return (
+//     <h1>¡{props.saludo}, {props.nombre}!</h1>
+//   )
+// }
+
+// function whitSaludo(WrappedComponent) {
+//   return function WrappedComponentWithSaludo (saludo) {
+//     return function ComponenteDeVerdad(props) {
+//       return <>
+//         <WrappedComponent {...props} saludo={saludo} />
+//         <p>Estamos acompañando al WrappedComponent</p>
+//       </>
+//     }
+//   }
+// }
+
+// const AppWithSaludo = whitSaludo(App)('Wenas');
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+// root.render(<App saludo={'Hola'} nombre={'Oier'}/>);
+// root.render(<AppWithSaludo saludo="Hola" nombre="Oier"/>);
